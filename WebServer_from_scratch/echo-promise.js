c2c191b7b6671264566a35908ea74e1d296e22e3
mlewis2257 <mlang.lewis@gmail.com>
2025-05-09 01:13:11 -0700

    echo promises

--- WebServer_from_scratch/echo-promise.js
+++ WebServer_from_scratch/echo-promise.js
@@ -1,2 +1,75 @@
 const net = require("net");
-const socket = net.Socket;
+const { buffer } = require("stream/consumers");
+// const socket = net.Socket()
+
+const soInit = (socket) => {
+  const conn = {
+    socket: socket,
+    reader: null,
+    ended: Boolean,
+    err: Error,
+  };
+
+  socket.on("data", (data) => {
+    console.assert(conn.reader);
+    socket.pause(); //Pause recieving more data until we're ready
+    conn.reader.resolve(data); // Resolves the pending promise
+    conn.reader = null;
+  });
+
+  socket.on("end", () => {
+    conn.ended = true;
+    if (conn.reader) {
+      conn.reader.resolve(buffer.from(""));
+      conn.reader = null;
+    }
+  });
+
+  socket.on("error", (error) => {
+    conn.err = error;
+    if (conn.reader) {
+      conn.reader.reject(err);
+      conn.reader = null;
+    }
+  });
+
+  return conn;
+};
+
+const soRead = (conn) => {
+  console.assert(!conn.reader); //Ensure no other read is in progress
+  return new Promise((resolve, reject) => {
+    // If the connection is not readable, complete new promise
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+    if (conn.ended) {
+      resolve(buffer.from("")); //EOF
+      return;
+    }
+    conn.reader = { resolve, reject };
+    conn.socket.resume();
+  });
+};
+
+const soWrite = (conn = conn, data = buffer) => {
+  console.assert(data.length > 0);
+  return new Promise((resolve, reject) => {
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+    conn.socket.write(data, (err) => {
+      if (err) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const server = net.createConnection({
+  pauseOnConnect: true,
+});
